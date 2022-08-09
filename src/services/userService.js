f71224dcd38a7a82e59e314260c19f2792fef1c0
KhoaConn <87683531+KhoaConn@users.noreply.github.com>
2022-08-09 23:43:50 +0700

    CRUD function

--- src/services/userService.js
+++ src/services/userService.js
@@ -8,4 +8,15 @@ const getAllUsers = (inputId) => {
     return axios.get('/api/get-all-users?id=${inputId}');
 }
 
-export { handleLoginApi, getAllUsers }
+const addNewUser = (data) => {
+    return axios.post('/api/create-new-user', data)
+}
+
+const deleteUserService = (userId) => {
+    return axios.delete('/api/delete-user', { data: { id: userId } })
+}
+
+const editUserService = (inputData) => {
+    return axios.put('/api/edit-user', inputData)
+}
+export { handleLoginApi, getAllUsers, addNewUser, deleteUserService, editUserService }
